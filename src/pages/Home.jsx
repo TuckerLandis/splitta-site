3b4302bdc5ff03bb224814ae3ecf0eff8d0f1967
Tucker Landis <78769701+TuckerLandis@users.noreply.github.com>
2025-01-29 09:55:07 -0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,45 @@
 import React from "react";
 import { motion } from "framer-motion";
+import ReactPlayer from "react-player";
 import "./Home.css"; // Import the CSS file
 
 const Home = () => {
   return (
-    <motion.div
-      className="home-container"
-      initial={{ opacity: 0, scale: 0.8, x: -300, y: 0 }}
-      animate={{ opacity: 1, scale: 1, x: -150, y: -275 }}
-      transition={{ duration: 1 }}
-    >
-      <motion.h1
-        className="vertical-text"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0, rotate: [0, 10, -10, 0] }}
-        transition={{ duration: 1, type: "spring", stiffness: 100 }}
-        whileHover={{ scale: 1.1 }}
+    <div>
+      <ReactPlayer
+        url="/assets/video/Scans3.mp4"
+        playing
+        loop
+        muted
+        width="100%"
+        height="100%"
+        className="video-background"
+      />
+      <motion.div
+        className="home-container"
+        initial={{ opacity: 0, scale: 0.8, x: -300, y: 0 }}
+        animate={{ opacity: 1, scale: 1, x: -150, y: -275 }}
+        transition={{ duration: 1 }}
       >
-        SPLITTA
-      </motion.h1>
-      {/* <motion.p
-        className="vertical-text"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, type: "spring", stiffness: 100 }}
-      >
-        Feel free to browse our website and learn more about us.
-      </motion.p> */}
-    </motion.div>
+        <motion.h1
+          className="vertical-text"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0, rotate: [0, 10, -10, 0] }}
+          transition={{ duration: 1, type: "spring", stiffness: 100 }}
+          whileHover={{ scale: 1.1 }}
+        >
+          SPLITTA
+        </motion.h1>
+        {/* <motion.p
+          className="vertical-text"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, type: "spring", stiffness: 100 }}
+        >
+          Feel free to browse our website and learn more about us.
+        </motion.p> */}
+      </motion.div>
+    </div>
   );
 };
 
